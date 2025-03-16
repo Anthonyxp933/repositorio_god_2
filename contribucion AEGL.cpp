9712b73e7c7dc335882e440a4ebc20523f472a42
Anthonyxp933 <agodoyl4@miumg.edu.gt>
2025-03-16 12:33:41 -0600

    prueba

---
+++ contribucion AEGL.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+class AEGL{
+	private : 
+    	string cui,nombre,apellidos;
+    	
+    	void crear();
+    	void leer();
+    	void actualizar();
+    	void borrar();
+};
